6bafd417e04136ce31f9f9c421674504db3fe558
Ewixxx <tranquility0122@gmail.com>
2023-09-13 20:09:29 +0800

    update function js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 import{initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {addition} from "../function.js"
+import {add} from "../function.js"
 import {getDatabase} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://maczelapizza-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
-console.log (addition(2,3))
+console.log (add(2,3))
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 
