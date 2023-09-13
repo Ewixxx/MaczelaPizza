2aa4e95292671d5339d8ce2739bde48bed041ed2
Ewixxx <tranquility0122@gmail.com>
2023-09-13 21:54:35 +0800

    update search and button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import{initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import {add} from "./function.js"
-import {getDatabase} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://maczelapizza-default-rtdb.asia-southeast1.firebasedatabase.app/"
@@ -10,10 +10,15 @@ console.log (add(2,3))
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 
+const pizzaDB =ref (database,"Pizza")
+
+
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click",function(){
 let inputValue = inputFieldEl.value
  console.log('${inputValue} added to database')
+ push(pizzaDB, inputValue)
 })
