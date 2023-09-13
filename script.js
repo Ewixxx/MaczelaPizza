8a497ac02248156680bd2752b21667265e2c9aaa
Ewixxx <tranquility0122@gmail.com>
2023-09-13 19:28:20 +0800

    get database

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
 import{initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {addition} from "../function"
-
+import {addition} from "../function.js"
+import {getDatabase} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://maczelapizza-default-rtdb.asia-southeast1.firebasedatabase.app/"
