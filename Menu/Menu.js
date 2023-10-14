65764412b150bd197dfa0bb873364b522cca9582
Ewixxx <130546265+Ewixxx@users.noreply.github.com>
2023-10-14 09:09:20 +0800

    new menu look

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,28 +1,69 @@
-import{initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const product = [
+  {
+      id: 0,
+      image: '1.jpg',
+      title: 'Bacon Mushroom',
+      price: 120,
+  },
+  {
+      id: 1,
+      image: '2.jpg',
+      title: 'Pizza Combo',
+      price: 60,
+  },
+  {
+      id: 2,
+      image: '3.jpg',
+      title: 'Pizza 3',
+      price: 230,
+  },
+  {
+      id: 3,
+      image: '4.jpg',
+      title: 'The Circle of Life',
+      price: 100,
+  },
+  {
+      id: 4,
+      image: '5.jpg',
+      title: 'All Time Favorite',
+      price: 230,
+  },
+  {
+      id: 5,
+      image: '6.jpg',
+      title: 'Mac Duo',
+      price: 100,
+  },
+];
 
-const appSettings = {
-  databaseURL: "https://maczelapizza-default-rtdb.asia-southeast1.firebasedatabase.app/"
-}
+const categories = [...new Set(product.map((item) => { return item }))]
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
+document.getElementById('searchBar').addEventListener('keyup', (e) => {
+  const searchData = e.target.value.toLowerCase();
+  const filteredData = categories.filter((item) => {
+      return (
+          item.title.toLowerCase().includes(searchData)
+      )
+  })
+  displayItem(filteredData)
+});
 
-const pizzaDB = ref (database,"Pizza")
-const inputFieldEl = document.getElementById("input-field").value.toUpperCase();
-
-
-// Pressing Enter key in Search bar
-let input = document.querySelector('input');
-input.addEventListener('keyup', (e) => {
-    if(e.keyCode === 13) {
-      let inputValue = inputFieldEl.value
-      console.log(`${inputValue} added to database`)
-      push(pizzaDB, inputValue)
-      inputFieldEl.value=""
-      cartListEl.innerHTML += `<li>${inputValue}</li>`
-     
-    }
-     })
-    
-
+const displayItem = (items) => {
+  document.getElementById('root').innerHTML = items.map((item) => {
+      var { image, title, price } = item;
+      return (
+          `<div class='box'>
+              <div class='img-box'>
+                  <img class='images' src=${image}></img>
+              </div> 
+              <div class='bottom'>
+                  <p>${title}</p>
+                  <h2> ${price}.00</h2>
+              <button>Add to cart</button>
+              </div>
+          </div>`
+      )
+  }).join('')
+};
+displayItem(categories);
