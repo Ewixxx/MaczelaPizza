c26abf2f730cf0bb632247732cd8fbecc3008d7e
Ewixxx <130546265+Ewixxx@users.noreply.github.com>
2023-10-16 18:04:13 +0800

    update menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,94 +1,67 @@
-let list = document.querySelector('.sidebody');
-let listCard = document.querySelector('.listCard');
-let total = document.querySelector('.total');
-let quantity = document.querySelector('.quantity');
+const searchInput = document.getElementById("search");
+const menuItems = document.querySelectorAll(".menu-item");
+const cartContainer = document.getElementById("cart-container");
+const cartItemsList = document.getElementById("cart-items");
 
-const product = [
-  {
-      id: 0,
-      image: '1.jpg',
-      title: 'Bacon Mushroom',
-      price: 120,
-  },
-  {
-      id: 1,
-      image: '2.jpg',
-      title: 'Pizza Combo',
-      price: 60,
-  },
-  {
-      id: 2,
-      image: '3.jpg',
-      title: 'Pizza 3',
-      price: 230,
-  },
-  {
-      id: 3,
-      image: '4.jpg',
-      title: 'The Circle of Life',
-      price: 100,
-  },
-  {
-      id: 4,
-      image: '5.jpg',
-      title: 'All Time Favorite',
-      price: 230,
-  },
-  {
-      id: 5,
-      image: '6.jpg',
-      title: 'Mac Duo',
-      price: 100,
-  },
-];
-
-const categories = [...new Set(product.map((item) => { return item }))]
-
-document.getElementById('searchBar').addEventListener('keyup', (e) => {
-  const searchData = e.target.value.toLowerCase();
-  const filteredData = categories.filter((item) => {
-      return (
-          item.title.toLowerCase().includes(searchData)
-      )
-  })
-  displayItem(filteredData)
+menuItems.forEach((menuItem) => {
+    const addToCartButton = menuItem.querySelector(".add-to-cart");
+    addToCartButton.addEventListener("click", () => {
+        addToCart(menuItem);
+    });
 });
 
-const displayItem = (items) => {
-  document.getElementById('root').innerHTML = items.map((item) => {
-      var { image, title, price } = item;
-      return (
-          `<div class='box'>
-              <div class='img-box'>
-                  <img class='images' src=${image}></img>
-              </div> 
-              <div class='bottom'>
-                  <p>${title}</p>
-                  <h2> ${price}.00</h2>
-              <button onclick="addItems(${title,price})">Add to cart</button>
-              </div>
-          </div>`
-      )
-  }).join('')
+searchInput.addEventListener("input", () => {
+    filterItems(searchInput.value.toLowerCase());
+});
 
-};
-displayItem(categories);
+function addToCart(menuItem) {
+    const name = menuItem.getAttribute("data-name");
+    const price = parseFloat(menuItem.getAttribute("data-price"));
+
+    // Check if the item is already in the cart
+    const existingCartItem = cartItemsList.querySelector(`[data-name="${name}"]`);
+    if (existingCartItem) {
+        // If it exists, increment the quantity and update the price
+        const quantityElement = existingCartItem.querySelector(".quantity");
+        const quantity = parseInt(quantityElement.textContent);
+        const newQuantity = quantity + 1;
+        quantityElement.textContent = newQuantity;
+        const totalPriceElement = existingCartItem.querySelector(".total-price");
+        const total = price * newQuantity;
+        totalPriceElement.textContent = `$${total.toFixed(2)}`;
+    } else {
+        // If it's a new item, add it to the cart
+        const cartItem = document.createElement("li");
+        cartItem.classList.add("cart-item");
+        cartItem.setAttribute("data-name", name);
+        cartItem.innerHTML = `
+            <img src="${menuItem.querySelector('img').getAttribute('src')}" alt="${name}">
+            <span>${name}</span>
+            <span class="quantity">1</span>
+            <span class="total-price">$${price.toFixed(2)}</span>
+            <button class="remove-from-cart">Remove</button>
+        `;
+        cartItemsList.appendChild(cartItem);
 
 
-// Cart Item
-
-const cart = [];
-function addItems(title,price) {
-    const item = {title,price};
-    cart.push(item);
-    updateCartDisplay();
-}
-function updateCartDisplay() {
-    const cartList = document.getElementById('cart');
-    cartList.innerHTML = '';
-    for (const item of cart) {
-        const li = document.createElement('li');
-        li.textContent = `${item.title} - $${item.price.toFixed(2)}`;
-        cartList.appendChild(li);
+        const removeFromCartButton = cartItem.querySelector(".remove-from-cart");
+        removeFromCartButton.addEventListener("click", () => {
+            cartItemsList.removeChild(cartItem);
+        });
     }
+
+    // Show the cart container
+    cartContainer.style.display = "block";
 }
+
+function filterItems(query) {
+    menuItems.forEach((menuItem) => {
+        const name = menuItem.getAttribute("data-name").toLowerCase();
+        if (name.includes(query)) {
+            menuItem.style.display = "inline-block";
+        } else {
+            menuItem.style.display = "none";
+        }
+    });
+}
+
