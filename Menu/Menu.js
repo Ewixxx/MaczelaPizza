9947a11f8a67cd449ae18acde9def527ee65365c
Ewixxx <130546265+Ewixxx@users.noreply.github.com>
2023-10-23 19:05:41 +0800

    functional add to cart button

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,31 +1,51 @@
 
 let cartlist = document.getElementById("cart-container")
+let billing = document.getElementById("billing")
 const Cart = document.getElementById("cart-icon")
 const Close = document.getElementById("closebtn")
+const checkout = document.getElementById("checkout")
+const cancelbtn =document.getElementById("cancelbtn")
+
+
+// Click the cart icon to collapse the cartlist
 Cart.addEventListener("click", function(){
     cartlist.classList.add("popcart");
 
 })
-
+// Use ESC button on keyboard to close the cartlist
 document.addEventListener("keydown", function(event) {
     if (event.key === "Escape") {
         cartlist.classList.remove("popcart");
+        billing.classList.remove("pop-billing");
     }
 })
+
 Close.addEventListener("click", function(){
     cartlist.classList.remove("popcart");
 })
+
+checkout.addEventListener("click",function(){
+    billing.classList.add("pop-billing");
+})
+cancelbtn.addEventListener("click",function(){
+    billing.classList.remove("pop-billing");
+})
+
+
+
 
 const searchInput = document.getElementById("search");
 const menuItems = document.querySelectorAll(".menu-item");
 const cartContainer = document.getElementById("cart-container");
 const cartItemsList = document.getElementById("cart-items");
+const totalAmountElement = document.getElementById("total-amount");
+
+let totalAmount = 0; // Initialize the total amount
 
 menuItems.forEach((menuItem) => {
     const addToCartButton = menuItem.querySelector(".add-to-cart");
     addToCartButton.addEventListener("click", () => {
         addToCart(menuItem);
-
     });
 });
 
@@ -48,7 +68,7 @@ function addToCart(menuItem) {
         quantityElement.textContent = newQuantity;
         const totalPriceElement = existingCartItem.querySelector(".total-price");
         const total = price * newQuantity;
-        totalPriceElement.textContent = `$${total.toFixed(2)}`;
+        totalPriceElement.textContent = `₱${total.toFixed(2)}`;
     } else {
         // If it's a new item, add it to the cart
         const cartItem = document.createElement("li");
@@ -56,24 +76,33 @@ function addToCart(menuItem) {
         cartItem.setAttribute("data-name", name);
         cartItem.innerHTML = `
             <img src="${menuItem.querySelector('img').getAttribute('src')}" alt="${name}">
-            <span>${name}</span>
+            <span>${name}"</span>
             <span class="quantity">1</span>
-            <span class="total-price">$${price.toFixed(2)}</span>
+            <span class="total-price">₱${price.toFixed(2)}</span>
             <button class="remove-from-cart">Remove</button>
         `;
         cartItemsList.appendChild(cartItem);
 
-
         const removeFromCartButton = cartItem.querySelector(".remove-from-cart");
         removeFromCartButton.addEventListener("click", () => {
+            // When an item is removed, update the total amount
             cartItemsList.removeChild(cartItem);
+            const total = price * parseInt(cartItem.querySelector(".quantity").textContent);
+            totalAmount -= total;
+            totalAmountElement.textContent = `₱${totalAmount.toFixed(2)}`;
         });
     }
+
+    // Update the total amount when adding an item to the cart
+    totalAmount += price;
+    totalAmountElement.textContent = `₱${totalAmount.toFixed(2)}`;
 
     // Show the cart container
     cartContainer.style.display = "block";
 }
 
+
+// Search Bar Function
 function filterItems(query) {
     menuItems.forEach((menuItem) => {
         const name = menuItem.getAttribute("data-name").toLowerCase();
@@ -87,49 +116,52 @@ function filterItems(query) {
 
 
 
-function calculateTotal() {
-    const totalAmount = document.querySelector("#total-amount");
-    const total = menuItems.reduce((acc, menuItems) => acc + menuItems.price, 0);
-    totalAmount.textContent = total.toFixed(2);
-}
-
 
 
 // // Event listener for submitting the order
-// const submitOrderButton = document.querySelector("#submit-order");
-// submitOrderButton.addEventListener("click", () => {
-//     // Get billing information
-//     const name = document.querySelector("#name").value;
-//     const email = document.querySelector("#email").value;
-//     const phone = document.querySelector("#phone").value;
-//     const address = document.querySelector("#address").value;
+const submitOrderButton = document.querySelector("#submit-order");
+submitOrderButton.addEventListener("click", () => {
+    // Get billing information
+    
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const phone = document.querySelector("#phone").value;
+    const address = document.querySelector("#address").value;
 
-//     // Validate billing information
-//     if (!name || !email || !phone || !address) {
-//         alert("Please fill out all billing information.");
-//         return;
-//     }
+    // Validate billing information
+    if (!name || !email || !phone || !address) {
+        alert("Please fill out all billing information.");
+        return;
+    }
+    else{
+        alert("Thank you for ordering!");
+        cartlist.style.display = "none";
+        billing.style.display = "none";
+    }
 
-//     // Display the order summary
-//     const orderSummaryList = document.querySelector("#order-summary-list");
-//     orderSummaryList.innerHTML = "";
-//     const product = document.getElementById("");
-//     menuItems.forEach(name => {
-//         const orderItem = document.createElement("li");
-//         orderItem.textContent = menuItems.name;
-//         orderSummaryList.appendChild(orderItem);
-//     });
-
-//     // Calculate and display the total amount
-//     calculateTotal();
-
-//     // Hide the billing section and show the order summary
-//     document.querySelector(".billing").style.display = "visible";
-//     document.querySelector(".order-summary").style.display = "block";
-// });
+});
 
 
 
+// Smooth Scrolling Effect
+$(document).ready(function(){
 
+    $("a").on('click', function(event) {
+  
 
+      if (this.hash !== "") {
 
+        event.preventDefault();
+  
+
+        var hash = this.hash;
+
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
